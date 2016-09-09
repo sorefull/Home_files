76cd92b3f858c29c6103ffa2659ea1a6d2de38f2
Oleg Cherednichenko <olegh.cherednichenko@gmail.com>
2016-09-09 13:00:44 +0300

    loading files

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  root 'welcome#index'
+  get 'about', to: 'welcome#about', as: 'welcome_about'
+
+  # get 'welcome/about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'post_comments/:post_id', to: 'admins#post_comments', as: 'admins_post_comments'
+end
