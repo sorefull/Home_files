4598f9d4ca4065b314f1a1242fe3fa0ed69e99e0
sorefull <olegh.cherednichenko@gmail.com>
2016-09-09 20:20:32 +0300

    adding users, design and final idea

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'about', to: 'welcome#about', as: 'welcome_about'
 
-  # get 'welcome/about'
+  devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'post_comments/:post_id', to: 'admins#post_comments', as: 'admins_post_comments'
