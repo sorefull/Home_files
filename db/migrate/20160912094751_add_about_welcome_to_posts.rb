1e6ca3c5c14fa4cff4c0e1b76eaade816472ccbc
Oleg Cherednichenko <olegh.cherednichenko@gmail.com>
2016-09-12 12:54:16 +0300

    migration truble

--- db/migrate/20160912094751_add_about_welcome_to_posts.rb
+++ db/migrate/20160912094751_add_about_welcome_to_posts.rb
@@ -1,4 +1,4 @@
-class AddAboutWelcomeAndAboutUserToPosts < ActiveRecord::Migration[5.0]
+class AddAboutWelcomeToPosts < ActiveRecord::Migration[5.0]
   def change
     add_column :posts, :about_welcome, :boolean, default: false
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911121547) do
+ActiveRecord::Schema.define(version: 20160912094751) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.integer  "user_id"
     t.string   "image"
+    t.boolean  "about_welcome", default: false
   end
 
   create_table "users", force: :cascade do |t|
